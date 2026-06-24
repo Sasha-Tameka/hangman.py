b1590e18dcddc656b803d1358f93252402c962b7
Sasha-Tameka <sashh92@hotmail.com>
2026-06-24 15:22:14 -0400

    fixed bug, shows how many tries are left

--- hangman.py
+++ hangman.py
@@ -1,23 +1,27 @@
 import random
 
-#Create a list of words
 word_list = ["aardvark", "baboon", "camel", "duck", "elephant", "fish", "giraffe", "hippo", "iguana", "jellyfish", "koala", "lion", "monkey", "ostrich", "penguin", "quetzal", "rhinoceros", "sloth", "tiger", "unicorn", "vulture", "wombat", "xerus", "yak", "zebra"]
 
-#Chose a random word
 chosen_word = random.choice(word_list)
 guessed_letters = []
 tries = 0
-
-#Display as underscores
-display = [ '_' for _ in chosen_word]
+display = ['_' for _ in chosen_word]
 
 print("Welcome to Hangman! You have 6 tries to guess the word.")
-print ("".join(display))
+print("".join(display))
 
-#Main game loop
 while True:
-    guess = input("Guess a letter: ").lower()
+    guess = input("\nGuess a letter: ").lower()
+
+    if not guess.isalpha() or len(guess) != 1:
+        print("Invalid input. Please enter a single letter (A-Z).")
+        continue
+    if guess in guessed_letters:
+        print("You already guessed that letter. Try again.")
+        continue
+
     guessed_letters.append(guess)
+
     if guess in chosen_word:
         for i in range(len(chosen_word)):
             if chosen_word[i] == guess:
@@ -26,28 +30,13 @@ while True:
     else:
         print("Wrong guess. Try again.\n--------------------------------------")
         tries += 1
+
     if tries == 6:
         print("You lost! The word was", chosen_word)
         break
     if "_" not in display:
         print("You won! The word was", chosen_word)
         break
-    
-    #Validations
-    while True:
-        guess = input("\nGuess a letter: ").lower()
-        if not guess.isalpha():
-            print("Invalid input. Please enter a letter (A-Z).")
-        continue
-        if len(guess) != 1:
-            print("Invalid input. Please enter a single letter.")
-            continue
-        if guess in guessed_letters:
-            print("You already guessed that letter. Try again.")
-            continue
-        break   
 
-    #End of game
     print("Guessed letters:", ", ".join(guessed_letters))
     print("Tries left:", 6 - tries)
-    
